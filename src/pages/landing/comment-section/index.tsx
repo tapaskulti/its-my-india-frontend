2ab2c2846cadfd67cca18b04610164bdfce7130e
Tapas Chandra <tapaskulti@gmail.com>
2025-03-08 19:54:41 +0530

    comments

--- src/pages/landing/comment-section/index.tsx
+++ src/pages/landing/comment-section/index.tsx
@@ -11,6 +11,28 @@ const DATA = [
       "It is simply a WOW.  At my age (84+ years) I did not wonder what was hidden from us for the past 75+ years. It is must read and must-have book for all Bharatiyas/Indians.",
   },
   {
+    name: "Giriraj Jain",
+    // designation: "Silicon Valley, California",
+    location: "Silicon Valley, California",
+    comment:
+      "I was pleasantly surprised.  Thank you for putting the foundation together for a case for renewal of the Constitution."
+  },
+  {
+    name: "G.S. Satya",
+    designation: "Visionary Entrepreneur",
+    location: "Silicon Valley, California",
+    comment:
+      `Dr. Ryan Baidya's monumental work, New Democratic Republic Constitution for India, is a timely and thought-provoking proposal for a revised and reimagined constitution for India, also known as Bharat. In this book, Dr. Baidya presents a comprehensive framework that covers:
+1.	The historical context and perspective of the existing constitution.
+2.	A critical analysis of its creation.
+3.	The omissions and commissions in the document.
+4.	Its relationship—or lack thereof—with Sanatana Samskruti (our ancient culture).
+5.	Appropriate changes for a modern, rapidly evolving world.
+6.	A roadmap for India's future as a global leader.
+….. borrowing from the words of Dr. Martin Luther King Jr.: “My fellow Bharatiyas, this proposed new constitution by Dr. Ryan Baidya is a marvelous ideal—a call to action that empowers you to rise as the Vishwa Guru (global leader) you are destined to be.”
+`
+  },
+  {
     name: "Uday Kumar Koppolu",
     designation: "CEO, Global Helping Hands",
     location: "Ongole, AP",
@@ -18,11 +40,32 @@ const DATA = [
       "…This was a good book, I liked it a lot. The writing was captivating.  I recommend every fellow citizen to read this book not only for themselves but also for their children's future.",
   },
   {
+    name: "Bivhash Chakraborty",
+    designation: "Retd. Ariforce",
+    location: "Durgapur, WB",
+    comment:
+      "I am not a reader-type of person, but this book kept me going.  We were kept in the dark for so long…",
+  },
+  {
+    name: "Pranab Phauzdar",
+    designation: "Artist",
+    location: "Kolkata",
+    comment:
+      "Beautifully knitted the facts and the situations to shed light on the issues that most us of never knew….",
+  },
+  {
     name: "Sai Prasad Venkatachalam",
     designation: "CEO, Sai Aashraya Foundation",
     location: "Bengaluru, Karnataka",
     comment:
-      "This book clearly showed that our Constitutional democracy is not secular, or socialist. The author rightly pointed out that the constitution in its current form is not up to the task of moving the nation forward as the citizens aspire for. It is a must-read book.",
+    `… This book clearly showed that our Constitutional democracy is not secular, or socialist.    The author rightly pointed out that the constitution in its current form is not up to the task of moving the nation forward as the citizens aspire for. It is a must-read book.`,
+  },
+  {
+    name: "Tapas Chandra",
+    designation: "Entrepreneur, Software Engineer",
+    location: "Kolkata, WB",
+    comment:
+    `… This book clearly showed that our Constitutional democracy is not secular, or socialist.    The author rightly pointed out that the constitution in its current form is not up to the task of moving the nation forward as the citizens aspire for. It is a must-read book.`,
   },
 ];
 
