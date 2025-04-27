f9184914b8b5f6911e40cd4706de93a7b66808ef
Tapas Chandra <tapaskulti@gmail.com>
2025-04-27 19:30:13 +0530

    the book new content

--- src/pages/the-book/index.tsx
+++ src/pages/the-book/index.tsx
@@ -19,7 +19,7 @@ India’s current Constitution, shaped by colonial-era laws and governance model
 
 
             <div className="leading-8">
-            <h2 className="font-medium pb-1">Key Topics Covered:</h2>
+            <h2 className="text-lg font-semibold pb-1">Key Topics Covered:</h2>
               <div className="flex items-center space-x-2">
                 <BookOpenText className="w-5 h-auto" />
                 <span>The colonial roots of India’s Constitution</span>
@@ -46,7 +46,7 @@ India’s current Constitution, shaped by colonial-era laws and governance model
             </div>
 
             <div className="my-4 leading-8">
-              <h2 className="font-medium pb-1">Table of Contents:</h2>
+              <h2 className="text-lg font-semibold pb-1">Table of Contents:</h2>
               <div className="flex items-center space-x-2">
                 <BookOpenText className="w-5 h-auto" />
                 <span>What is India?</span>
@@ -98,7 +98,7 @@ India’s current Constitution, shaped by colonial-era laws and governance model
             <div className="text-2xl pt-20 pb-8 text-center">A New Constitution for India</div>
 
             <div className="my-4 leading-8">
-              <h2 className="font-medium pb-1">Why need changes?</h2>
+              <h2 className="text-lg font-semibold pb-1">Why need changes?</h2>
               <div className="flex items-center space-x-2">
                 <CheckCheck className="w-5 h-auto" />
                 <span>The current Constitution still reflects British-era governance, suppressing India’s true potential.</span>
@@ -115,7 +115,7 @@ India’s current Constitution, shaped by colonial-era laws and governance model
             </div>
 
             <div className="my-4 leading-8">
-              <h2 className="font-medium pb-1">Proposed Reforms</h2>
+              <h2 className="text-lg font-semibold pb-1">Proposed Reforms</h2>
               <div className="flex items-center space-x-2">
                 <CheckCheck className="w-5 h-auto" />
                 <span>A New Constitution based on Bharat’s cultural and historical ethos</span>
@@ -142,7 +142,7 @@ India’s current Constitution, shaped by colonial-era laws and governance model
             </div>
 
             <div className="my-4 leading-8">
-              <h2 className="font-medium pb-1">The Time to Act is Now</h2>
+              <h2 className="text-lg font-semibold pb-1">The Time to Act is Now</h2>
               <div className="flex items-center space-x-2">
                 <CheckCheck className="w-5 h-auto" />
                 <span>Join the movement to demand a new Constitution for Bharat!</span>
@@ -150,7 +150,7 @@ India’s current Constitution, shaped by colonial-era laws and governance model
             </div>
 
             <div className="my-4 leading-8">
-              <h2 className="font-medium pb-1">Perspective</h2>
+              <h2 className="text-lg font-semibold pb-1">Perspective</h2>
               <h2>The Constitution of India, often hailed as a remarkable achievement of post-colonial governance, stands as a testament to the nation's commitment to democratic principles.     However, the passage of time and the complexities of modern governance have revealed both the strengths and the limitations of this revered document.<br /><br />
               This book is a timely and thought-provoking exploration of India's constitutional journey. The author delves into the very heart of what it means to be a sovereign nation, challenging us to reconsider whether the Constitution, as it stands today, truly reflects the ethos and aspirations of the Indian people.<br /><br />
               This book is not just an academic critique; it is a passionate call for a re-evaluation of the foundations upon which India's governance is built. It argues that for India to move forward, it must shed the remnants of its colonial past and embrace a constitutional framework that is genuinely rooted in its history, culture, and values.<br /><br />
@@ -159,60 +159,147 @@ India’s current Constitution, shaped by colonial-era laws and governance model
             </div>
 
             <div className="my-4 leading-8">
-              <h2 className="font-medium pb-1">A Bold Reimagining of India's Constitutional Future</h2>
+              <h2 className="text-lg font-semibold pb-1">A Bold Reimagining of India's Constitutional Future</h2>
               <h2>A New Democratic Republic Constitution for India by Ryan Baidya is a striking and thought-provoking proposal for constitutional reform in India. Through the lens of India’s colonial past and current governance challenges, Baidya offers an unapologetic and in-depth analysis of why the present Constitution, despite its promises of democracy, secularism, and socialism, has fallen short of creating an equitable, just society. His central thesis is clear: for India to achieve its true potential, it must discard its colonial-era Constitution and embrace a new document that reflects its indigenous values, history, and contemporary aspirations.
               </h2>
             </div>
 
             <div className="my-4 leading-8">
-              <h2 className="font-medium pb-1">Decolonizing India at the Core</h2>
+              <h2 className="text-lg font-semibold pb-1">Decolonizing India at the Core</h2>
               <h2>Baidya’s critique is built around the concept of decolonization—not just of the legal frameworks but of the very psyche of India’s governance. He meticulously traces the influence of colonial-era laws, such as the Indian Penal Code, and how these remnants still shape and, in his view, limit India's independence. The author argues that the current Indian Constitution is, at its core, a document of compromise, influenced by British colonial rulers and designed to keep India within the constraints of its past. For India to truly thrive, Baidya proposes a “New Freedom Day” and a "New Constitution" to symbolize a complete break from colonial influences.
               </h2>
             </div>
 
             <div className="my-4 leading-8">
-              <h2 className="font-medium pb-1">The Need for a New Constitution</h2>
+              <h2 className="text-lg font-semibold pb-1">The Need for a New Constitution</h2>
               <h2>Through an extensive exploration of India’s constitutional evolution, the book builds a strong case for why the current framework has not delivered on its socialist and democratic promises. Economic inequality, political corruption, and religious tensions are rampant. Baidya dives into the structural inadequacies of the Indian Constitution that perpetuate wealth disparities and hinder true secularism and democracy. His analysis points to how certain articles and provisions within the Constitution have been insufficient or misused, allowing power and wealth to concentrate among a select few.
               </h2>
             </div>
 
             <div className="my-4 leading-8">
-              <h2 className="font-medium pb-1">An Indigenous Approach to Governance</h2>
+              <h2 className="text-lg font-semibold pb-1">An Indigenous Approach to Governance</h2>
               <h2>One of the book's strengths is its focus on indigenous systems of governance. In Baidya’s vision, India's future must be rooted in its rich historical traditions, such as the Vedic systems of governance, which valued the role of leaders as servants of the people. Baidya offers detailed proposals for integrating ancient Indian values into a modern constitutional framework that embraces diversity, ensures accountability, and promotes prosperity for all citizens.
               </h2>
             </div>
 
             <div className="my-4 leading-8">
-              <h2 className="font-medium pb-1">The Symbolic Importance of December 30, 1943</h2>
+              <h2 className="text-lg font-semibold pb-1">The Symbolic Importance of December 30, 1943</h2>
               <h2>One of the book’s key proposals is the revision of India’s Independence Day to December 30, 1943. Baidya argues that this was the true moment of sovereignty, marked by Subhas Chandra Bose’s hoisting of the Indian flag in the Andaman and Nicobar Islands, symbolizing India’s first assertion of full independence. This idea is indicative of Baidya’s desire to break free from the traditional narratives of Indian independence, encouraging a rethinking of historical milestones.
               </h2>
             </div>
 
             <div className="my-4 leading-8">
-              <h2 className="font-medium pb-1">A Visionary Blueprint</h2>
+              <h2 className="text-lg font-semibold pb-1">A Visionary Blueprint</h2>
               <h2>The most compelling part of A New Democratic Republic Constitution for India comes in the later chapters, where Baidya outlines his vision for the proposed new Constitution. This new document aims to eliminate colonial legacies, establish a robust justice system, and create a "Police of Police" oversight mechanism to curb corruption and abuse of power among public servants. The author also touches on reforming the electoral system to ensure true majority representation in government, arguing for candidates to win more than 50% of registered voters' support.
               </h2>
             </div>
 
             <div className="my-4 leading-8">
-              <h2 className="font-medium pb-1">Critical Reforms and Practical Solutions</h2>
+              <h2 className="text-lg font-semibold pb-1">Critical Reforms and Practical Solutions</h2>
               <h2>Baidya doesn’t limit himself to theoretical musings. He offers concrete solutions, such as overhauling the penal system, rethinking the role of police, and introducing stricter safeguards for citizens' rights. His focus on women's rights, particularly concerning combating violence and introducing special units for justice in rape cases, shows a commitment to practical social reform.
               </h2>
             </div>
 
             <div className="my-4 leading-8">
-              <h2 className="font-medium pb-1">A Call to Action</h2>
+              <h2 className="text-lg font-semibold pb-1">A Call to Action</h2>
               <h2>In its essence, the book is a clarion call for citizens, policymakers, intellectuals, and leaders to engage in the dialogue of constitutional reform. Baidya’s work isn’t just a critique of the present but a forward-thinking blueprint for a prosperous and sovereign India. He makes it clear that this change won’t come from small tweaks but requires a complete overhaul of the nation’s governing documents.
               </h2>
             </div>
 
             <div className="my-4 leading-8">
-              <h2 className="font-medium pb-1">Final Thoughts</h2>
+              <h2 className="text-lg font-semibold pb-1">Final Thoughts</h2>
               <h2>A New Democratic Republic Constitution for India is a well-researched, intellectually engaging, and visionary work. It pushes readers to think deeply about the future of India’s democracy, its governance, and its standing in the world. Baidya's passionate arguments for decolonization and the introduction of an indigenous, people-centered Constitution make this a timely and relevant book for anyone interested in the evolution of governance in India.<br /><br />
               If there’s any critique, it’s that some proposals may seem too radical to implement within the current political landscape, but Baidya’s boldness is part of the book’s charm. It’s not a proposal for half-measures but for revolutionary change.<br /><br />
               For readers who are passionate about political reform, governance, and the future of Indian democracy, this book is a must-read.
 
               </h2>
+            </div>
+
+            <div className="my-4">
+              <h2 className="text-lg font-semibold pb-1 border-dashed border-b-2 border-slate-600">Detailed Contents</h2>
+              
+              <div className="py-5">
+                <h2 className="text-base text-black font-semibold">PART - I</h2>
+                <div className="mt-2">
+                  <h2 className="text-base text-gray-700 font-semibold">Chapter I: What is India?</h2>
+                  <span className="pl-20 text-gray-700">Characterizations of the Indian constitution</span><br />
+                  <h2 className="text-base text-gray-700 font-semibold">Chapter II: Birth of the Indian Republic and its Constitution</h2>
+                </div>
+              </div>
+
+              <div className="py-3">
+                <h2 className="text-base text-black font-semibold">PART - II</h2>
+                <div className="mt-2">
+                  <h2 className="text-base text-gray-700 font-semibold">Chapter III: The Actual Date of Sovereignty</h2>
+                  <h2 className="text-base text-gray-700 font-semibold">Chapter IV: British Ruler’s Penal Codes and its Direct</h2>
+                </div>
+              </div>
+
+              <div className="py-3">
+                <h2 className="text-base text-black font-semibold">PART - III</h2>
+                <div className="mt-2">
+                  <h2 className="text-base text-gray-700 font-semibold">Chapter-V: The Drafting of the Indian Constitution:</h2>
+                  <span className="pl-[5.5rem] text-gray-700">Influences and Imperatives</span><br />
+                  <h2 className="text-base text-gray-700 font-semibold">Chapter VI: The Evolution of India's Constitution:</h2>
+                  <span className="pl-[5.75rem] text-gray-700">A Blend of Colonial Influence and Indian Elite</span><br />
+                  <h2 className="text-base text-gray-700 font-semibold">Chapter VII: Major Influences on the Indian Constitution</h2>
+                  <h2 className="text-base text-gray-700 font-semibold">Chapter-VIII: British East India Company Laws and Regulations in Today’s Indian Constitution</h2>
+                  <h2 className="text-base text-gray-700 font-semibold">Chapter-IX: Indigenous Governance Systems in India were mostly Sidelined</h2>
+                  <h2 className="text-base text-gray-700 font-semibold">Chapter-X: Vedic System of Governance</h2>
+                  <span className="pl-[5.5rem] text-gray-700">The role of the Rajan (king) as a servant of the people, not merely a ruler</span><br />
+                </div>
+              </div>
+
+              <div className="py-3">
+                <h2 className="text-base text-black font-semibold">PART - IV</h2>
+                <div className="mt-2">
+                  <h2 className="text-base text-gray-700 font-semibold">Chapter–XI: A would-have-been Constitution Based on Ancient Indian Laws,</h2>
+                  <h2 className="text-base text-gray-700 font-semibold">Chapter–XII: Azad Hind Constitution</h2>
+                </div>
+              </div>
+
+              <div className="py-3">
+                <h2 className="text-base text-black font-semibold">PART - V</h2>
+                <div className="mt-2">
+                  <h2 className="text-base text-gray-700 font-semibold">Chapter-XIII: Naming the Country</h2>
+                  <h2 className="text-base text-gray-700 font-semibold">Chapter–XIV: The Perils of Adopting Colonial Constitutions:</h2>
+                  <h2 className="text-base text-gray-700 font-semibold">Chapter-XV: The Indian Constitution Was Not Truly "By the People"</h2>
+                  <h2 className="text-base text-gray-700 font-semibold">Chapter–XVI:  Clarifying the Preamble:</h2>
+                  <span className="pl-[6.75rem] text-gray-700">Addressing Historical Discrepancies and Legal Accuracy</span><br />
+                  <h2 className="text-base text-gray-700 font-semibold">Chapter–XVII: The Indian Constitution IS not a Socialist</h2>
+                  <h2 className="text-base text-gray-700 font-semibold">Chapter–XVIII: The Indian Constitution IS not Democratic in its True Sense</h2>
+                  <h2 className="text-base text-gray-700 font-semibold">Chapter–XIX: Secularism and Constitution</h2>
+                  <span className="pl-[6.75rem] text-gray-700">The current Constitution of India has failed as truly secular.</span><br />
+                  <h2 className="text-base text-gray-700 font-semibold">Chapter – XX: The Marginalized Majority: Revisiting India’s Constitutional Legacy</h2>
+                </div>
+              </div>
+
+              <div className="py-3">
+                <h2 className="text-base text-black font-semibold">PART - VI</h2>
+                <div className="mt-2">
+                  <h2 className="text-base text-gray-700 font-semibold">Chapter–XXI: The Imperative of Constitutional Renewal</h2>
+                  <h2 className="text-base text-gray-700 font-semibold">Chapter–XXII: A Constitution is not a fixed document.</h2>
+                  <h2 className="text-base text-gray-700 font-semibold">Chapter–XXIII: New Aspirations Need New Constitution of Free India</h2>
+                  <h2 className="text-base text-gray-700 font-semibold">Chapter–XXIV: Several Specific Proposed Revisions to the Indian Constitution</h2>
+                  <h2 className="text-base text-gray-700 font-semibold">Chapter–XXV: A New Constitution for India</h2>
+                  <h2 className="text-base text-gray-700 font-semibold">Chapter–XXVI: Critical Analysis of the New Constitution</h2>
+                  <h2 className="text-base text-gray-700 font-semibold">Chapter–XXVII: India’s New Constitution and Amended Article 368: A Path to New Bharat</h2>
+                  <h2 className="text-base text-gray-700 font-semibold">Chapter–XXVIII: Healing Fractured Bharat: Through Constitutional Renewal</h2>
+                </div>
+              </div>
+
+              <div className="py-3">
+                <h2 className="text-base text-black font-semibold">APPENDICES</h2>
+                <div className="mt-2">
+                  <h2 className="text-base text-gray-700 font-semibold">Appendix–I: The importance of connecting to one's roots for national resilience and identity</h2>
+                  <h2 className="text-base text-gray-700 font-semibold">Appendix–II: Redoing the Bharatiya Nyaya Sanhita (BNS)</h2>
+                  <h2 className="text-base text-gray-700 font-semibold">Appendix–III: Proposal for the Establishment of  an Independent <b>"Police of Police"</b> System</h2>
+                  <h2 className="text-base text-gray-700 font-semibold">Appendix–IV: How to deter and prevent rape</h2>
+                  <h2 className="text-base text-gray-700 font-semibold">Appendix–V: Prime Minister Election Process;</h2>
+                  <span className="pl-[6.75rem] text-gray-700">Chief Minister Election Process</span>
+                </div>
+              </div>
+
             </div>
 
             </div>
